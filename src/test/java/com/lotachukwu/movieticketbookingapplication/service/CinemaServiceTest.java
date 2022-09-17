47d1e240ea36ad19e61d3649bb03585144ac8ed3
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-09-17 23:40:04 +0100

    working ....

---
+++ src/test/java/com/lotachukwu/movieticketbookingapplication/service/CinemaServiceTest.java
@@ -0,0 +1,17 @@
+package com.lotachukwu.movieticketbookingapplication.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CinemaServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
