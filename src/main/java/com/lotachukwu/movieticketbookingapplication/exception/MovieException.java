40aecf99522797cc335e1218ad31ab0aefe15186
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-09-23 13:03:21 +0100

    implementing functionalities

---
+++ src/main/java/com/lotachukwu/movieticketbookingapplication/exception/MovieException.java
@@ -0,0 +1,8 @@
+package com.lotachukwu.movieticketbookingapplication.exception;
+
+public class MovieException extends RuntimeException{
+
+    public MovieException(String message) {
+        super(message);
+    }
+}
